6d32a5074fde9e19cc08e744d9c07696f7a7d8fd
Gouravvyas04 <gouravvys542@gmail.com>
2024-04-05 23:10:39 +0530

    update

--- timer.js
+++ timer.js
@@ -4,7 +4,9 @@ let restart = document.querySelector(".restart");
 let hours = document.querySelector(".hours");
 let minutes = document.querySelector(".minutes");
 let seconds = document.querySelector(".seconds");
-let s, m, h;
+let s = 0,
+  m = 0,
+  h = 0;
 let interval;
 
 start.addEventListener("click", function () {
@@ -13,45 +15,47 @@ start.addEventListener("click", function () {
 });
 
 stop.addEventListener("click", function () {
-  console.log("close");
   clearInterval(interval);
-  s = 0;
-  m = 0;
-  h = 0;
 });
-
+//Restart button
 restart.addEventListener("click", function () {
   clearInterval(interval);
   seconds.textContent = "00";
   minutes.textContent = "00:";
   hours.textContent = "00:";
-  (s = 0), (m = 0), (h = 0);
+  s = 0;
+  m = 0;
+  h = 0;
   starttime();
 });
 
 function starttime() {
-  s = 0;
   interval = setInterval(function () {
     s++;
-    seconds.textContent = s;
-    if (s <= 10) {
-      s.textContent = "0" + s;
+    if (s < 10) {
+      seconds.textContent = "0" + s;
+    } else {
+      seconds.textContent = s;
     }
+
     if (s === 60) {
       m++;
       s = 0;
-      if (m <= 10) {
-        minutes.textContent = "0" + m;
+      if (m < 10) {
+        minutes.textContent = "0" + m + ":";
+      } else {
+        minutes.textContent = m + ":";
       }
-      minutes.textContent = m;
     }
+
     if (m === 60) {
       h++;
       m = 0;
-      if (h <= 10) {
-        hours.textContent = "0" + h;
+      if (h < 10) {
+        hours.textContent = "0" + h + ":";
+      } else {
+        hours.textContent = h + ":";
       }
-      hours.textContent = h;
     }
   }, 1000);
 }
